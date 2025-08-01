f2d468cc36b7a66d93b5b1363ecbf32c6016c822
Marko Markov <stormshadow973@gmail.com>
2025-08-01 11:39:32 +0300

    Remove forgotten debug logs

--- src/gui/orderbook_cache.cpp
+++ src/gui/orderbook_cache.cpp
@@ -6,18 +6,11 @@ extern OKXClient* g_okx_client;
 
 std::future<nlohmann::json> OrderbookCache::fetch_orderbook_async(const std::string& instId, std::string& error) {
     return std::async(std::launch::async, [instId, &error]() {
-        printf("[Orderbook] Request for instId: %s\n", instId.c_str());
         std::string local_error;
         if (!g_okx_client) {
-            printf("[Orderbook] g_okx_client is nullptr!\n");
             return nlohmann::json();
         }
         nlohmann::json ob = g_okx_client->fetch_orderbook(instId, local_error);
-        if (!local_error.empty()) {
-            printf("[Orderbook] ERROR for %s: %s\n", instId.c_str(), local_error.c_str());
-        } else if (!ob.contains("data") || !ob["data"].is_array() || ob["data"].empty()) {
-            printf("[Orderbook] EMPTY orderbook for %s\n", instId.c_str());
-        }
         error = local_error;
         return ob;
     });
